9fdf74e7567f3ea71d2f6cdbaa3bc59ba165a9e3
yousefmasry4 <masry011546699917>
2020-02-24 01:46:39 +0200

    init

--- my_codes/Sum of minimum absolute difference of each array element.cpp
+++ my_codes/Sum of minimum absolute difference of each array element.cpp
@@ -2,3 +2,88 @@
 // Created by youssef on ٢٤‏/٢‏/٢٠٢٠.
 //
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumAbsoluteDifference function below.
+int minimumAbsoluteDifference(vector<int> arr) {
+   // Initialize difference as infinite 
+   int diff = INT_MAX; 
+   int len=arr.size();
+   sort(arr.begin(),arr.end());
+   for(int i=0;i <len ; i++){
+       for(int j=i+1;j<len;j++){
+           int temp=abs(arr[i]-arr[j]);
+           if(temp < diff){
+               diff=temp;
+           }else
+           {
+               break;
+           }
+       }
+   }
+   return diff;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = minimumAbsoluteDifference(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
