7218d25e3495fec61316ec4ec09713684824f4da
yousefmasry4 <masry011546699917>
2020-02-25 17:11:08 +0200

    init

---
+++ AI/Bot saves princess/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+def displayPathtoPrincess(n, grid):
+    moves = ["LEFT", "RIGHT", "UP", "DOWN"]
+    '''
+    (0,0)
+    
+    
+                (3,3)
+    
+    '''
+    mx, my = tuple(grid["p"])
+    bx, by  = tuple(grid["m"])
+    if (bx < mx):
+        for i in range(mx - bx):
+            print(moves[3])
+    elif (bx > mx):
+        for i in range(bx - mx):
+            print(moves[2])
+    if (by < my):
+        for i in range(my - by):
+            print(moves[1])
+    elif (by > my):
+        for i in range(by - my):
+            print(moves[0])
+
+
+m = int(input())
+loc = {}
+for i in range(0, m):
+    data = input().strip()
+    if 'm' in data:
+        loc["m"] = [i, data.index('m')]
+    if 'p' in data:
+        loc["p"] = [i, data.index('p')]
+displayPathtoPrincess(m, loc)
