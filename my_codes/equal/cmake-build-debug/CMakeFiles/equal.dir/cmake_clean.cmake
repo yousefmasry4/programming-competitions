69680ca16514f7d1fa48e016f4599ea7e89a75c4
yousefmasry4 <masry011546699917>
2020-02-25 15:13:55 +0200

    init

---
+++ my_codes/equal/cmake-build-debug/CMakeFiles/equal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/equal.dir/main.cpp.o"
+  "equal"
+  "equal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/equal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ my_codes/equal/main.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int equal(vector<int> a){
+    auto min_=*min_element(a.begin(),a.end());
+    auto ans=numeric_limits<int>::max();
+    for(auto i=0;i<5;i++){
+        ans=min(ans,accumulate(
+                    a.begin(),
+                    a.end(),
+                    0,
+                    [min_,i](int init,int j){
+                            auto t=j-(min_-i);
+                            return init+t/5+t%5/2+t%5%2;
+                        }
+                    )
+                );
+    }
+    return  ans;
+}
+
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string arr_temp_temp;
+        getline(cin, arr_temp_temp);
+
+        vector<string> arr_temp = split_string(arr_temp_temp);
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++) {
+            int arr_item = stoi(arr_temp[i]);
+
+            arr[i] = arr_item;
+        }
+
+        int result = equal(arr);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
