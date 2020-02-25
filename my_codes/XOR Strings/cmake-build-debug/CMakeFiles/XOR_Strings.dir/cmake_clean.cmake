e87d0fa0f3e597335c83130c65fcb26a1472034c
yousefmasry4 <masry011546699917>
2020-02-25 16:33:00 +0200

    init

---
+++ my_codes/XOR Strings/cmake-build-debug/CMakeFiles/XOR_Strings.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XOR_Strings.dir/main.cpp.o"
+  "XOR_Strings"
+  "XOR_Strings.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XOR_Strings.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
